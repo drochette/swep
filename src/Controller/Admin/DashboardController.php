13266b271f0ce8f88e2ff200b1eebe929f0f2786
Damien Rochette <damien.rochette@aramisauto.com>
2026-01-21 10:29:06 +0100

    feat: add easyadmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Campus;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[AdminDashboard(routePath: '/admin', routeName: 'app_admin_dashboard')]
+class DashboardController extends AbstractDashboardController
+{
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Administration')
+        ;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-star');
+        yield MenuItem::linkToRoute('Page d\'accueil', 'fa fa-home', 'app_home');
+        yield MenuItem::linkToCrud('Campus', 'fas fa-home', Campus::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+    }
+}
